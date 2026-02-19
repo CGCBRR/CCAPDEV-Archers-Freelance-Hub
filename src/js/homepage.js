afa2e45c2b51b0d2d76ca8f9399b215d8c86ed99
DaniDev937 <daniel_rebudiao@dlsu.edu.ph>
2026-02-19 12:02:29 +0800

    cleaned + updated categories review section

--- src/js/homepage.js
+++ src/js/homepage.js
@@ -5,7 +5,3 @@ function openPopup() {
 function closePopup() {
   document.getElementById("overlay").style.display = "none";
 }
-
-function writeReview() {
-  document.getElementById("write-review").style.display = "flex";
-}
